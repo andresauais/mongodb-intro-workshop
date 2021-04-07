355408da42f5245e65bc9062a463e320fc9556be
Andres Auais <andresauais@gmail.com>
2021-04-07 15:04:31 +0100

    ex1 done

--- src/db/connect.js
+++ src/db/connect.js
@@ -15,6 +15,13 @@ const mongoose = require("mongoose");
  *   useUnifiedTopology: true,
  * }
  */
-function connect() {}
+function connect() {
+  return mongoose.connect("mongodb://localhost:27017/myApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+}
 
 module.exports = connect;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const app = require("./server");
 const config = require("./config/config");
+const connect = require("./db/connect");
 
 app.listen(config.app.PORT, () => {
+  config.logger.info(`DB connected`);
+  
   config.logger.info(`Server running at http://localhost:${config.app.PORT}`);
 });
